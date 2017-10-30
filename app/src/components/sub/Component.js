d25f7aee1cc7c915da102434a3dd696236d89f98
francesco.casula <francesco.casula@dhigroupinc.com>
2017-10-30 13:51:33 +0000

    Adding @Component decoration to super class

--- app/src/components/sub/Component.js
+++ app/src/components/sub/Component.js
@@ -1,5 +1,7 @@
 import Vue from 'vue';
+import VueComponent from 'vue-class-component';
 
+@VueComponent
 export default class Component extends Vue {
     message = 'Hello from parent component';
 
